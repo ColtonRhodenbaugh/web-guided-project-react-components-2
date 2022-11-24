22e3856cd47e8d77c133ceaea7104a0c2b2e3240
Colton Rhodenbaugh <coltonrhodenbaugh@Coltons-Air.hsd1.il.comcast.net>
2022-11-23 18:00:42 -0600

    fully done

--- src/components/App.js
+++ src/components/App.js
@@ -7,18 +7,26 @@ import Search from './Search'
 // 👉 2- Import the dummy data that will power the application.
 // (Tomorrow we'll fetch the data from an API instead.)
 import friendsData, { hello } from "../dummy-data/friends";
-console.log(hello);
+
 
 export default function App() {
   // 👉 3- Initialize a slice of state to keep track of the data
   // using the dummy data as the initial value of the slice of state
-
+  const [friends, setFriends] = useState(friendsData);
   // 👉 4- Initialize a slice to keep track of the value of the search box
   // using an empty string as the initial value of the slice
-
+  const [searchTerm, setSearchTerm] = useState('');
   // 👉 5- Build a `changeStatus` function that takes an id and
   // changes the `married` from true to false and viceversa
-
+  const changeStatus = id => {
+    setFriends(friends.map(fr => {
+      if(fr.id == id) {
+        return { ...fr, married: !fr.married }
+      }else{
+        return fr
+      }
+    }))
+  }
   // STRETCH - Make a helper function that returns
   // a filtered array of friends data (filtering by search term)
 
@@ -26,9 +34,10 @@ export default function App() {
     <div className='app-friends container'>
       {/* 👉 6- Render the Search component */}
       {/* STRETCH - Changes to the input should update the search term */}
-
+      < Search />
       {/* 👉 7- Render the FriendsList component */}
       {/* What prop/props does FriendsList need? */}
+      < FriendsList friends={friends} changeStatus = {changeStatus} />
     </div>
   )
 }
